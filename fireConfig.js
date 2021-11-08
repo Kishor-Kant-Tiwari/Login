721d8101d5964a26e8e397c4fd22f20397bb2664
Kishor-Kant-Tiwari <kishor.tiwari_cs20@gla.ac.in>
2021-11-08 11:17:20 +0530

    new

---
+++ fireConfig.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAM1SVGYkEvmHx8PsM-llG0FGCcLTdIngU",
+  authDomain: "login-1f1f4.firebaseapp.com",
+  databaseURL: "https://login-1f1f4-default-rtdb.firebaseio.com",
+  projectId: "login-1f1f4",
+  storageBucket: "login-1f1f4.appspot.com",
+  messagingSenderId: "913042052421",
+  appId: "1:913042052421:web:f404f15d9413b73e959a2c"
+};
+
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
---
+++ script1.js
@@ -0,0 +1,99 @@
+var card=document.getElementById("card");         
+function openRegister()
+     {
+     card.style.transform = "rotateY(-180deg)";
+     
+     
+     }	
+function openLogin()
+     {
+         card.style.transform = "rotateY(0deg)";
+         
+         	
+     }
+
+
+
+var nameV;
+var emailV;
+var mobileV;
+var passwordV; 
+var randomKey=parseInt(10+(1000-10)*Math.random());
+function readfom()
+{
+var randomKey=parseInt(10+(1000-10)*Math.random());
+nameV = document.getElementById('name').value;
+emailV = document.getElementById('email').value;
+mobileV = document.getElementById('mobile').value;
+passwordV = document.getElementById('password').value;
+
+console.log(nameV, emailV, mobileV, passwordV);
+}
+
+// Reset Password
+function ResetPassword(){
+  var auth=firebase.auth();
+  var resetEmail=document.getElementById('emailReset').value;
+   if(resetEmail !=""){
+     auth.sendPasswordResetEmail(resetEmail).then(function(){
+     alert("Email has been sent !")
+     }).catch(function(error){
+      var errorCode= error.code;
+      var errMessage=error.message;
+     alert("Message:"+errMessage);
+     });
+   }else{
+     alert("Please Enter Your Email");
+   }
+
+}
+function readLoginData(){
+  
+  _username=document.getElementById('_email').value;
+  _password=document.getElementById('_password').value;
+  
+
+  if(_username != "" && _password != ""){
+   // console.log(_password+_username);
+     var result = firebase.auth().signInWithEmailAndPassword(_username,_password);
+     alert("You have Successful Login");
+     result.catch(function(error){
+       var errorCode= error.code;
+       var errMessage=error.message;
+      alert("Message:"+errMessage);
+     });
+  }else{
+    alert("Please Enter Username & Password");
+  }
+}
+
+// Register Worl
+
+document.getElementById('Register').onclick = function() 
+{
+      readfom();
+      
+      console.log(_password+_email);
+       var result = firebase.auth().createUserWithEmailAndPassword(emailV,passwordV);
+       firebase.database().ref('Student/'+randomKey).set({
+       name: nameV,
+       email: emailV,
+       mobile: mobileV,
+       password: passwordV,
+       
+       });
+       result.catch(function(error){
+        var errorCode= error.code;
+        var errMessage=error.message;
+        alert("Message:"+errMessage);
+      });
+      
+    
+    
+     document.getElementById("name").value = "";
+     document.getElementById("email").value = "";
+     document.getElementById("mobile").value = "";
+     document.getElementById("password").value = "";
+}
+
+
