7db807a067edf73dedbc1524b9f1b6cfcb6f23fc
Kishor-Kant-Tiwari <kishor.tiwari_cs20@gla.ac.in>
2021-11-24 06:08:59 +0530

    few

--- script1.js
+++ script1.js
@@ -56,7 +56,7 @@ function readLoginData(){
   if(_username != "" && _password != ""){
    // console.log(_password+_username);
      var result = firebase.auth().signInWithEmailAndPassword(_username,_password);
-     window.location.href = "http://stackoverflow.com";
+     window.open("http://stackoverflow.com");
    
      result.catch(function(error){
        var errorCode= error.code;
